622f18fe6f05920da73307a6de1381b87b6da247
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-05-23 21:24:51 +0200

    Transformado MutableState a MutableStateFlow en DetailViewModel

--- app/src/main/java/com/aiglepub/architectcoders/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/aiglepub/architectcoders/ui/screens/detail/DetailScreen.kt
@@ -1,7 +1,6 @@
 package com.aiglepub.architectcoders.ui.screens.detail
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +18,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -28,10 +29,9 @@ import com.aiglepub.architectcoders.data.Movie
 import com.aiglepub.architectcoders.ui.ScreenAppTheme
 import com.aiglepub.architectcoders.ui.common.LoadingProgressIndicator
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(vm: DetailViewModel = viewModel(), onBack: () -> Unit) {
-    val state = vm.state
+    val state by vm.state.collectAsState()
 
     ScreenAppTheme {
         Scaffold(
@@ -50,7 +50,6 @@ fun DetailScreen(vm: DetailViewModel = viewModel(), onBack: () -> Unit) {
             }
         }
     }
-
 }
 
 @Composable
--- app/src/main/java/com/aiglepub/architectcoders/ui/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/aiglepub/architectcoders/ui/screens/detail/DetailViewModel.kt
@@ -1,24 +1,27 @@
 package com.aiglepub.architectcoders.ui.screens.detail
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aiglepub.architectcoders.data.Movie
 import com.aiglepub.architectcoders.data.MoviesClient
 import com.aiglepub.architectcoders.data.MoviesRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class DetailViewModel(private val id: Int): ViewModel() {
     private val repository = MoviesRepository(MoviesClient.instance)
-    var state by mutableStateOf(UiState())
-        private set
+    //var state by mutableStateOf(UiState())
+    //    private set
+
+    private val _state: MutableStateFlow<UiState> = MutableStateFlow(UiState())
+    val state: StateFlow<UiState> = _state.asStateFlow()
 
     init {
         viewModelScope.launch {
-            state = UiState(loading = true)
-            state = UiState(loading = false, movie = repository.findMovieById(id))
+            _state.value = UiState(loading = true)
+            _state.value = UiState(loading = false, movie = repository.findMovieById(id))
         }
     }
     data class UiState(
