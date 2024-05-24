487ea2b65801f0ba45a522c749dbea129462123b
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-05-25 00:35:29 +0200

    Agregada navegacion de las 3 formas

--- app/src/main/java/com/aiglepub/architectcoders/ui/navigation/Navigation.kt
+++ app/src/main/java/com/aiglepub/architectcoders/ui/navigation/Navigation.kt
@@ -7,20 +7,51 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import com.aiglepub.architectcoders.ui.screens.detail.DetailScreen
 import com.aiglepub.architectcoders.ui.screens.home.HomeScreen
-import com.aiglepub.architectcoders.data.movies
 import com.aiglepub.architectcoders.ui.screens.detail.DetailViewModel
+import com.aiglepub.architectcoders.ui.screens.home.HomeViewModel
 
+
+///METODO DE NAVEGACION CON NAVIGATION COMPOSE
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Home ) {
+        composable<Home>{
+            HomeScreen(onClick = { movie ->
+                    navController.navigate(Detail(movie.id))
+                }
+            )
+        }
+
+        composable<Detail>(
+        ) {backStackEntry ->
+            val detail = backStackEntry.toRoute<Detail>()
+            DetailScreen(
+                vm = viewModel { DetailViewModel(detail.movieId) } ,
+                onBack = { navController.popBackStack(route = Home, inclusive = false)}
+            )
+        }
+    }
+}
+
+/// METODO ANTIGUO DE NAVEGACION
+/*
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "home" ) {
         composable("home") {
-            HomeScreen(onClick = {movie ->
-                navController.navigate("detail/${movie.id}")
-            })
+            HomeScreen(
+                vm = viewModel { HomeViewModel() },
+                onClick = {movie ->
+                    navController.navigate("detail/${movie.id}")
+                }
+            )
         }
 
         composable(
@@ -36,3 +67,50 @@ fun Navigation() {
         }
     }
 }
+ */
+
+
+
+
+/// METODO DE NAVEGACION CON SEALED CLASS
+/*
+sealed class NavScreen(val route: String) {
+    data object Home : NavScreen("home")
+    data object Detail : NavScreen("detail/{${NavArgs.MovieId.key}}") {
+        fun createRoute(movieId: Int) = "detail/$movieId"
+    }
+}
+
+enum class NavArgs(val key: String) {
+    MovieId("movieId")
+}
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavScreen.Home.route ) {
+        composable(NavScreen.Home.route) {
+            HomeScreen(
+                vm = viewModel { HomeViewModel() },
+                onClick = {movie ->
+                    navController.navigate(NavScreen.Detail.createRoute(movie.id))
+                }
+            )
+        }
+
+        composable(
+            route = NavScreen.Detail.route,
+            arguments = listOf(navArgument(NavArgs.MovieId.key) { type = NavType.IntType})
+        ) {backStackEntry ->
+            val movieId = backStackEntry.arguments?.getInt(NavArgs.MovieId.key)
+            requireNotNull(movieId)
+            DetailScreen(
+                vm = viewModel { DetailViewModel(movieId) } ,
+                onBack = { navController.popBackStack()}
+            )
+        }
+    }
+}
+*/
+
---
+++ app/src/main/java/com/aiglepub/architectcoders/ui/navigation/navDestination.kt
@@ -0,0 +1,8 @@
+package com.aiglepub.architectcoders.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home
+@Serializable
+data class Detail(val movieId: Int)
