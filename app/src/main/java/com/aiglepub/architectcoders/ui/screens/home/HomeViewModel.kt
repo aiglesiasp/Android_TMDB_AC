efad7f56ea973146a8e75d72176fe2876f1d76bd
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-07-20 00:22:14 +0200

    Transformando de flow a StateFlow en el HomeViewModel

--- app/src/main/java/com/aiglepub/architectcoders/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/aiglepub/architectcoders/ui/screens/home/HomeViewModel.kt
@@ -4,31 +4,51 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aiglepub.architectcoders.data.Movie
 import com.aiglepub.architectcoders.data.MoviesRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class HomeViewModel(
-    private val repository: MoviesRepository
-): ViewModel() {
+class HomeViewModel(repository: MoviesRepository): ViewModel() {
 
     //Variable de Estado Antigua
     //var state by mutableStateOf(UiState())
     //    private set
 
     // Variable de Estado con Flows
-    private val _state: MutableStateFlow<UiState> = MutableStateFlow(UiState())
-    val state: StateFlow<UiState> = _state.asStateFlow()
-    
+    //private val _state: MutableStateFlow<UiState> = MutableStateFlow(UiState())
+    //val state: StateFlow<UiState> = _state.asStateFlow()
+
+    //State Flow para hacer esperar el de peticion de peliculas, ya que puede ser que no hagamos aceptado la region
+    // y entonces hace la peticion de peliculas antes de tenerlas
+    private val uiReadyState = MutableStateFlow(false)
+
+    //DE Flow a StateFlow
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val state: StateFlow<UiState> = uiReadyState
+        .filter { it }
+        .flatMapLatest { repository.movies }
+        .map { UiState(movies = it) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = UiState(loading = true)
+        )
+
+
     fun onUiReady() {
-        viewModelScope.launch {
+        /*viewModelScope.launch {
             _state.value = UiState(loading = true)
             repository.movies.collect { listMovies ->
                 _state.value = UiState(loading = false, movies = listMovies)
             }
             //_state.value = UiState(loading = false, movies = repository.fetchPopularMovies())
-        }
+        }*/
+        uiReadyState.value = true
     }
 
     //Modelo de data class de estados
